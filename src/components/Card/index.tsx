1730e7ecc65a8c99604cc60eabb3ec0cda0d9753
Juan Pablo <juanpablo.codes@gmail.com>
2023-02-19 16:30:52 -0300

    fix: interaction user remove task

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,14 @@
 import { Feather, FontAwesome } from '@expo/vector-icons';
 import { format } from 'date-fns';
-import { Checkbox, Text, useToast } from 'native-base';
+import {
+  Alert,
+  Button,
+  Checkbox,
+  HStack,
+  Slide,
+  Text,
+  useToast,
+} from 'native-base';
 import { useEffect, useState } from 'react';
 import { PutFinishTask } from '../../services/tasks/requests';
 import { Actions, Container, Info, InfoItem, Tag } from './styles';
@@ -10,6 +18,7 @@ function Card({ title, tag, id, date, hour, finish, onDeleteTask }: CardProps) {
   const dateFormatted = new Date(date);
   const hourFormatted = new Date(hour);
   const [isFinish, setIsFinish] = useState(finish);
+  const [isOpenTop, setIsOpenTop] = useState(false);
   const toast = useToast();
 
   const time = format(hourFormatted, 'kk:mm');
@@ -52,8 +61,37 @@ function Card({ title, tag, id, date, hour, finish, onDeleteTask }: CardProps) {
           accessibilityLabel="tarefa finalizada."
         />
         <Text textDecorationLine={isFinish ? 'line-through' : ''}>{title}</Text>
+
+        <Slide in={isOpenTop} placement="top">
+          <Alert justifyContent="center" status="info" safeAreaTop={4}>
+            <Alert.Icon />
+            <Text color="blue.600" fontWeight="medium">
+              Você tem certeza que deseja excluir essa tarefa?
+            </Text>
+            <HStack justifyContent="space-evenly" w="100%">
+              <Button
+                variant="link"
+                size="sm"
+                colorScheme="blue"
+                onPress={() => setIsOpenTop(!isOpenTop)}
+              >
+                Deixar como está
+              </Button>
+              <Button
+                variant="outline"
+                colorScheme="dark"
+                size="sm"
+                bgColor="#006AFF"
+                onPress={() => onDeleteTask(id)}
+              >
+                Excluir
+              </Button>
+            </HStack>
+          </Alert>
+        </Slide>
+
         <FontAwesome
-          onPress={() => onDeleteTask(id)}
+          onPress={() => setIsOpenTop(!isOpenTop)}
           name="trash-o"
           size={24}
           color="red"
